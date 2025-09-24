534d2ad7867b7716b90be029100a451929a45d47
Afonso Torres <afonsotorres953@gmail.com>
2025-09-24 02:46:07 +0100

    FrontEnd
    Buttons =true
    TextBox Input/Output = true
    Logic and Backend = False

---
+++ Calculadora/Calculadora1/MainWindow.axaml.cs
@@ -0,0 +1,17 @@
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using System;
+
+namespace Calculadora1;
+
+public partial class MainWindow : Window
+{
+    public string input = "";
+    public MainWindow()
+    {
+        InitializeComponent();
+        
+        bt0.Click => (,)
+            /
+}
+}
