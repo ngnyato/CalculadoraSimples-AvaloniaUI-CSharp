0d944fae60d3faa1708a86b89d426ead12ca31ed
ngnyato <afonsotorres953@gmail.com>
2025-09-25 04:23:56 +0100

    v0.2 Logic Update
    Already does some operations with 1 operator in a expression only
    operators available are:
    + - / *
    It Also have support for 0.1 etc
    It converts . to , for system understanding

--- Calculadora/Calculadora1/MainWindow.axaml.cs
+++ Calculadora/Calculadora1/MainWindow.axaml.cs
@@ -1,48 +1,121 @@
 using System.Diagnostics;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
+using System.Globalization;
 
 namespace Calculadora1;
 
 public partial class MainWindow : Window
 {
-    public string input = "";
-    public TextBox output;
+    private string input = "";
+    private TextBox output; // caxa de txt para meter a expressao
 
     public MainWindow()
     {
         InitializeComponent();
-
-        if (output == null){
-         output = this.FindControl<TextBox>("Display");
+        if (output == null)  // isto é para caso a txtBox nao estiver atribuida ele atribui a uma txt box que tenha o name de Display
+        {
+            output = this.FindControl<TextBox>("Display");
         }
-
-
     }
 
-    private void Btn_Click(object? sender, RoutedEventArgs e)
+    private void Btn_Click(object? sender, RoutedEventArgs e)    // apenas bts com Click="Btn_Click" vao dar trigger nisto
     {
-        if (sender is Button b)
+        if (sender is Button b)  // se o sender for um buttao é def como buttao b que vai servir de variavel para qual seja o nosso sender
         {
+
+
+            // CLEAR
             if (b.Content == "C")
             {
-                input = "";
-                Debug.WriteLine("igual");
+             ClearDisplay();
+             return; // evita continuar a concatenar, ou seja, vai fzr com que por tu teres clicado o buttao C ele nao adicione o C ao display
             }
-            else if (b.Content == "=")
+
+            // IGUAL
+            if (b.Content == "=")
             {
-                 // Aqui vao os calculos
-                 Debug.WriteLine("resultados");
-                 
+                char ch = ' ';
+                var s = input.Replace(" ", "");
+
+                // encontra o primeiro operador (ignora '-' no início)
+                int pos = -1;
+                char op = ' ';
+                
+                    for (int i = 1; i < s.Length; i++)
+                    {
+                        ch = s[i];
+                        if (ch == '+' || ch == '-' || ch == '*' || ch == '/')
+                        {
+                            pos = i;
+                            op = ch;
+                        }
+                    }
+                    if (pos == -1)
+                    {
+                        output.Text = "Operador inválido";
+                        return;
+                    }
+
+                
+
+                string left = s[..pos];
+                string right = s[(pos + 1)..];
+
+                if (!double.TryParse(left.Replace(',', '.'), NumberStyles.Float, CultureInfo.InvariantCulture,
+                        out var a) ||
+                    !double.TryParse(right.Replace(',', '.'), NumberStyles.Float, CultureInfo.InvariantCulture,
+                        out var bnum))
+                {
+                    output.Text = "Número inválido";
+                    input = "";
+                    return;
+                }
+
+                
+                double result = 0;
+                switch (op)
+                {
+                    case '+':
+                        result = a + bnum;
+                        break;
+                    case '/':
+                        result = a / bnum;
+                        break;
+                    case '*':
+                        result = a * bnum;
+                        break;
+                    case '-':
+                        result = a - bnum;
+                        break;
+
+                    default:
+                        output.Text = "Error";
+                        break;
+                }
+                output.Text = result.ToString();
+
             }
-            else
+
+            if (b.Content.ToString() != "C" && b.Content.ToString() != "=")
             {
-                input += b.Content?.ToString();
+                AdicionarChar(b.Content.ToString());
             }
-            output.Text = input;
         }
-        
     }
+    
+    private void AdicionarChar(string text)
+    {
+        input += text;
+        output.Text = input;
+    }
+
+        private void  ClearDisplay()
+        {
+            input = "";
+            output.Text = "";
+            Debug.WriteLine("clear");
+        }
 }
            
     
